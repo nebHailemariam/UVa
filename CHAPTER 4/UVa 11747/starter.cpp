ff574d15fd6b64b4dd5f5bb0f7a4fc10cc39a482
Nebiyou Hailemariam <nebHailemariam@gmail.com>
2020-08-10 17:41:28 +0300

    UVa 11747

---
+++ CHAPTER 4/UVa 11747/starter.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<pair<pair<int, int>, int>> EdgeList;
+
+typedef vector<int> vi;
+
+class UnionFind
+{
+private:
+    vi p, rank, setSize;
+    int numSets;
+
+public:
+    UnionFind(int N)
+    {
+        setSize.assign(N, 1);
+        numSets = N;
+        rank.assign(N, 0);
+        p.assign(N, 0);
+        for (int i = 0; i < N; i++)
+            p[i] = i;
+    }
+    int findSet(int i) {
+        return (p[i] == i) ? i : (p[i] = findSet(p[i]));
+    }
+    bool isSameSet(int i, int j) {
+        return findSet(i) == findSet(j);
+    }
+    void unionSet(int i, int j)
+    {
+        if (!isSameSet(i, j))
+        {
+            numSets--;
+            int x = findSet(i), y = findSet(j);
+            // rank is used to keep the tree short
+            if (rank[x] > rank[y])
+            {
+                p[y] = x;
+                setSize[x] += setSize[y];
+            }
+            else
+            {
+                p[x] = y;
+                setSize[y] += setSize[x];
+                if (rank[x] == rank[y])
+                    rank[y]++;
+            }
+        }
+    }
+    int numDisjointSets() {
+        return numSets;
+    }
+    int sizeOfSet(int i) {
+        return setSize[findSet(i)];
+    }
+};
+
+
+
+bool comp(pair<pair<int, int>, int> a, pair<pair<int, int>, int> b) {
+    if (a.second < b.second)return true;
+    return false;
+}
+
+void KruskalsAlgorithm(int m) {
+    sort(EdgeList.begin(), EdgeList.end(), comp);
+    UnionFind u(m);
+    int v = 0;
+    int i = 0;
+    int cost = 0;
+    vector<int> res;
+    while (v < m && i < EdgeList.size()) {
+        pair<pair<int, int>, int> front = EdgeList[i];
+        if (!u.isSameSet(front.first.first, front.first.second)) {
+            cost += front.second;
+            v++;
+            u.unionSet(front.first.first, front.first.second);
+
+        }
+        else {
+            res.push_back(front.second);
+        }
+        i++;
+    }
+    while (i < EdgeList.size()) {
+        pair<pair<int, int>, int> front = EdgeList[i];
+        res.push_back(front.second);
+        i++;
+    }
+    for (int i = 0; i < res.size();i++) {
+        if (i != 0)
+            cout<<" "<<res[i];
+        else
+            cout<<res[i];
+    }
+    if (res.size() == 0)
+        cout<<"forest";
+    cout<<"\n";
+    return;
+}
+
+int main()
+{
+    cin.sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    // freopen("in.txt", "r", stdin);
+    // freopen("out.txt", "w", stdout);
+
+    int m, n;
+    while (cin>>m>>n && (m != 0 || n != 0)) {
+        EdgeList = vector<pair<pair<int, int>, int>>();
+        int weight = 0;
+        while (n--) {
+            int u, v, w;
+            cin>>u>>v>>w;
+            weight+=w;
+            EdgeList.push_back(pair<pair<int, int>, int>(pair<int, int>(u, v), w));
+        }
+        KruskalsAlgorithm(m);
+    }
+}
